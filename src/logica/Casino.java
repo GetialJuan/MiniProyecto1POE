ed01d8ab37e6b99b322590c464763faba33a7423
GetialJuan <getial.juan@correounivalle.edu.co>
2022-05-07 18:26:29 -0500

    Clase Logicas(bases)

--- src/logica/Casino.java
+++ src/logica/Casino.java
@@ -4,10 +4,62 @@
  */
 package logica;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan
  */
-public class Casino {
+public class Casino 
+{
+    private List<Ronda> rondas;
+    private int rondaActual;
+    private int resuldadoDados1;
+    private int resuldadoDados2;
+    private int jugadorALanzar;
     
+    public Casino()
+    {
+        rondas = new ArrayList();
+        rondas.add(new Ronda());
+        rondaActual = 0;
+        jugadorALanzar = (int) (Math.random() * (2-1));
+    }
+    
+    public void lanzarDados()
+    {
+        int dado1 = (int) (Math.random() * (6-1));
+        int dado2 = (int) (Math.random() * (6-1));
+        
+        switch (jugadorALanzar) 
+        {
+            case 1:
+                resuldadoDados1 = dado1 + dado2;
+                jugadorALanzar = 2;
+                break;
+            case 2:
+                resuldadoDados2 = dado1 + dado2;
+                jugadorALanzar = 1;
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+    }
+    
+    public void sumarPuntajesDeLanzamiento()
+    {
+        if(resuldadoDados1 != resuldadoDados2)
+        {
+            rondas.get(rondaActual).sumarPuntajeAJugador(1, resuldadoDados1);
+            rondas.get(rondaActual).sumarPuntajeAJugador(2, resuldadoDados2);
+        }
+    }
+    
+    public void jugarOtraRonda()
+    {
+        rondas.add(new Ronda());
+        rondaActual += 1;
+    }
 }
--- src/logica/Ronda.java
+++ src/logica/Ronda.java
@@ -8,6 +8,64 @@ package logica;
  *
  * @author Juan
  */
-public class Ronda {
+public class Ronda 
+{
+    private int puntajeJugador1;
+    private int puntajeJugador2;
+    private int lanzamientosRealizadosJugador1;
+    private int lanzamientosRealizadosJugador2;
+    private int lanzamientosEmpatados;
+    private int lanzamientosARealizar;
+    
+    public Ronda()
+    {
+        puntajeJugador1 = 0;
+        puntajeJugador2 = 0;
+        lanzamientosEmpatados = 0;
+        lanzamientosRealizadosJugador1 = 0;
+        lanzamientosRealizadosJugador2 = 0;
+    }
+    
+    public void sumarPuntajeAJugador(int cualJugador, int puntajeASumar)
+    {
+        switch (cualJugador) {
+            case 1:
+                puntajeJugador1 += puntajeASumar;
+                añadirLanzamientoRealizadoAJugador(1);
+                break;
+            case 2:
+                puntajeJugador2 += puntajeASumar;
+                añadirLanzamientoRealizadoAJugador(2);
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+        restarLanzamientoARealizar();
+    }
+    
+    public void restarLanzamientoARealizar()
+    {
+        lanzamientosARealizar -= 1;
+    }
+    
+    public void añadirLanzamientoEmpatado()
+    {
+        lanzamientosEmpatados += 1;
+    }
+    
+    public void añadirLanzamientoRealizadoAJugador(int cualJugador)
+    {
+        switch (cualJugador) {
+            case 1:
+                lanzamientosRealizadosJugador1 += 1;
+                break;
+            case 2:
+                lanzamientosRealizadosJugador2 += 1;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
     
 }
